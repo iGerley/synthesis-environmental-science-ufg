57fd3a0ec692eb66b91cbe8a59941017c57a230f
iGerley <gerleyadrianomiranda@gmail.com>
2018-04-05 10:56:45 -0300

    Improved the code organization.

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,11 @@ module.exports = {
   module: {
     rules: [
 	  {
-	    loader: 'babel-loader',
-		test: /\.js$/,
-		exclude: /node_modules/
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
 	  }
-	]
+    ]
   },
   devtool: 'cheap-module-eval-source-map'
 };
