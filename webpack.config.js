3c8f0fbdf8c004953f46a33540fd644fe80794f6
iGerley <gerleyadrianomiranda@gmail.com>
2018-04-12 21:06:49 -0300

    Added the compress webpack dev parameter.

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
 	},
 	devtool: 'cheap-module-eval-source-map',
 	devServer: {
+		compress: true,
 		contentBase: path.resolve(__dirname, 'public'),
 		compress: true,
 		port: 3000
